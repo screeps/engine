2e87dc84406769d01b8490e3b989b22de93f6402
Russell Bryson <rbryson74@gmail.com>
2016-12-02 12:54:35 -0700

    Only check for a rampart at the same position

--- src/processor/intents/creeps/withdraw.js
+++ src/processor/intents/creeps/withdraw.js
@@ -23,7 +23,7 @@ module.exports = function(object, intent, roomObjects, roomTerrain, bulk, bulkUs
     if(!target) {
         return;
     }
-    if(object.user != target.user && _.any(roomObjects, i => i.type == C.STRUCTURE_RAMPART && i.user != object.user && !i.isPublic)) {
+    if(object.user != target.user && _.any(roomObjects, i => i.type == C.STRUCTURE_RAMPART && i.user != object.user && !i.isPublic && i.x == target.x && i.y == target.y)) {
         return;
     }
     if(Math.abs(target.x - object.x) > 1 || Math.abs(target.y - object.y) > 1) {
