4d0bc08ec067822429dcb7e11f83e6438034d063
Mark Bertels <markbertels@butads.nl>
2017-09-28 20:14:57 +0200

    Dropped an !

--- src/game/market.js
+++ src/game/market.js
@@ -18,7 +18,7 @@ exports.make = function(runtimeData, intents, register) {
             if(resourceType != 'all' && !_.contains(C.RESOURCES_ALL, resourceType) && resourceType != C.SUBSCRIPTION_TOKEN) {
                 return {};
             }
-            if(runtimeData.market.orders[resourceType]) {
+            if(!runtimeData.market.orders[resourceType]) {
                 return {};
             }
             cachedOrders[resourceType] = JSON.parse(JSON.stringify(runtimeData.market.orders[resourceType] || '{}'));
