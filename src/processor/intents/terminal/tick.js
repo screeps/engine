0c28eaa5c07f9636df11b3d8586c82f200d28cfb
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2018-01-03 23:51:59 +0300

    Fixed capacity of enemy terminals

--- src/processor/intents/terminal/tick.js
+++ src/processor/intents/terminal/tick.js
@@ -6,7 +6,7 @@ var _ = require('lodash'),
 module.exports = function(object, roomObjects, roomTerrain, bulk, bulkUsers, roomController, gameTime) {
 
     if(roomController) {
-        var energyCapacity = C.CONTROLLER_STRUCTURES.terminal[roomController.level] ? C.TERMINAL_CAPACITY : 0;
+        var energyCapacity = roomController.level > 0 && roomController.user == object.user && C.CONTROLLER_STRUCTURES.terminal[roomController.level] ? C.TERMINAL_CAPACITY : 0;
         if(energyCapacity != object.energyCapacity) {
             bulk.update(object, {energyCapacity});
         }
