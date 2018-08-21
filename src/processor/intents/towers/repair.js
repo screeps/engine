b849416ba685a545fd15dba617675e39273ac282
Artem Chivchalov <chivchalov@gmail.com>
2018-08-21 23:50:03 +0300

    fix(processor): add missing `eventLog` argument in tower repair intent

--- src/processor/intents/towers/repair.js
+++ src/processor/intents/towers/repair.js
@@ -3,7 +3,7 @@ var _ = require('lodash'),
     driver = utils.getDriver(),
     C = driver.constants;
 
-module.exports = function(object, intent, {roomObjects, bulk, stats}) {
+module.exports = function(object, intent, {roomObjects, bulk, stats, eventLog}) {
 
     if(object.type != 'tower') {
         return;
