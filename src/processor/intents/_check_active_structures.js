963b7112678e86983e28ec805dfcb768c81f8668
Sergey <mukhinserg88@gmail.com>
2019-03-18 00:06:25 +0200

    Update _check_active_structures.js

--- src/processor/intents/_check_active_structures.js
+++ src/processor/intents/_check_active_structures.js
@@ -10,7 +10,7 @@ module.exports = function (ownedObjects, { roomController, bulk }) {
     for (let type in ownedObjects) {
         var objects = ownedObjects[type];
 
-        if (objects.length > C.CONTROLLER_STRUCTURES[type][roomController.level | 0]) {
+        if (objects.length > C.CONTROLLER_STRUCTURES[type][roomController.level || 0]) {
             objects.sort(utils.comparatorDistance(roomController));
             objects = _.take(objects, C.CONTROLLER_STRUCTURES[type][roomController.level | 0]);
             ownedObjects[type].forEach(i => i._off = i.user != roomController.user || !_.contains(objects, i));
