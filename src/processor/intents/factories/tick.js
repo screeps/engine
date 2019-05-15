6dd60b66490b437ae1f2f0ba64e74b3a6d261f2d
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2019-05-15 23:52:30 +0300

    fix(processor): inactive factory capacity
    DEV-377

--- src/processor/intents/factories/tick.js
+++ src/processor/intents/factories/tick.js
@@ -3,7 +3,14 @@ const _ = require('lodash'),
     driver = utils.getDriver(),
     C = driver.constants;;
 
-module.exports = function(object, {gameTime,bulk}) {
+module.exports = function(object, {roomController,bulk}) {
+    if(roomController) {
+        var energyCapacity = roomController.level > 0 && roomController.user == object.user && C.CONTROLLER_STRUCTURES.factory[roomController.level] ? C.FACTORY_CAPACITY : 0;
+        if(energyCapacity != object.energyCapacity) {
+            bulk.update(object, {energyCapacity});
+        }
+    }
+
     if(!_.isEqual(object._actionLog, object.actionLog)) {
         bulk.update(object, {
             actionLog: object.actionLog
