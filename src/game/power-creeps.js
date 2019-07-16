6f321bb7269adeb120e990b4ac3e15360b298395
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2019-07-17 00:12:17 +0300

    hotfix(runtime): PowerCreep.usePower regression
    DEV-393

--- src/game/power-creeps.js
+++ src/game/power-creeps.js
@@ -272,7 +272,7 @@ exports.make = function(_runtimeData, _intents, _register, _globals) {
         if(_.isArray(ops)) {
             ops = ops[powerData.level-1];
         }
-        if((data(this.id).ops || 0) < ops) {
+        if((data(this.id).store.ops || 0) < ops) {
             return C.ERR_NOT_ENOUGH_RESOURCES;
         }
         if(powerInfo.range) {
