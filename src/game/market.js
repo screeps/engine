b75b1635d709fccd786b1b581419563196323508
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2019-09-28 21:29:28 +0300

    hotfix(processor): Market.deal regression
    DEV-393

--- src/game/market.js
+++ src/game/market.js
@@ -124,15 +124,15 @@ exports.make = function(runtimeData, intents, register) {
                 if(!terminal) {
                     return C.ERR_NOT_OWNER;
                 }
-                if(terminal[C.RESOURCE_ENERGY] < transferCost) {
+                if(!terminal.store || terminal.store[C.RESOURCE_ENERGY] < transferCost) {
                     return C.ERR_NOT_ENOUGH_RESOURCES;
                 }
                 if(terminal.cooldownTime > runtimeData.time) {
                     return C.ERR_TIRED;
                 }
                 if(order.type == C.ORDER_BUY) {
-                    if(order.resourceType != C.RESOURCE_ENERGY && (!terminal[order.resourceType] || terminal[order.resourceType] < amount) ||
-                         order.resourceType == C.RESOURCE_ENERGY && terminal[C.RESOURCE_ENERGY] < amount + transferCost) {
+                    if(order.resourceType != C.RESOURCE_ENERGY && (!terminal.store[order.resourceType] || terminal.store[order.resourceType] < amount) ||
+                         order.resourceType == C.RESOURCE_ENERGY && terminal.store[C.RESOURCE_ENERGY] < amount + transferCost) {
                         return C.ERR_NOT_ENOUGH_RESOURCES;
                     }
                 }
