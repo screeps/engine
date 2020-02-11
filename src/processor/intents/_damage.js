83dbcc3c50aa8a21831db46b2092bbbea59a8a21
Piotr Rogowski <piotrekrogowski@gmail.com>
2020-02-11 12:58:39 +0100

    fix(processor): use data object in EVENT_OBJECT_DESTROYED
    Every other event uses data subobject
    for keeping details about event except EVENT_OBJECT_DESTROYED
    This change makes that consistent with other events

--- src/processor/intents/_damage.js
+++ src/processor/intents/_damage.js
@@ -48,7 +48,9 @@ module.exports = function(object, target, damage, attackType, scope) {
 
             require('./structures/_destroy')(target, scope, attackType);
 
-            eventLog.push({event: C.EVENT_OBJECT_DESTROYED, objectId: target._id, type: object.type});
+            eventLog.push({event: C.EVENT_OBJECT_DESTROYED, objectId: target._id, data: {
+                type: object.type
+            }});
         }
     }
     else {
