5482a3f64618112ab0dcd04f69ddfba45e8f65bf
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2020-04-24 19:42:32 +0300

    ♻ move ptr flag to process.env
    DEV-536

--- src/config.js
+++ src/config.js
@@ -1,12 +1,8 @@
 var _ = require('lodash');
 var result = {};
 
-try {
-    result = require('./local-config');
-}
-catch(e) {}
-
 _.merge(result, {
+    ptr: "true" == process.env.PTR,
     redis: {
         port: 6379,
         host: '127.0.0.1',
